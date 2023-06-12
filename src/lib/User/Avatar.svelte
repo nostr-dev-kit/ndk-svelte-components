cf3c068924caeb320aa4d715f8766d28fb1ec0b3
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-06-12 15:04:09 +0200

    Add loading states to avatar and name

--- src/lib/User/Avatar.svelte
+++ src/lib/User/Avatar.svelte
@@ -27,13 +27,30 @@
         user = ndk.getUser(opts);
     }
 
-    let defaultStyle = 'width:64px; height: 64px;';
+    let defaultStyle = 'width:64px; height: 64px; background-color: #ccc;';
 </script>
 
-{#await user?.fetchProfile() then eventSet}
+{#await user?.fetchProfile()}
+    <img
+        src="https://placehold.co/400/ccc/ccc/webp"
+        alt={`Loading avatar for ${npub}`}
+        title={`Loading avatar for ${npub}`}
+        class={$$props.class}
+        style={!$$props.class && !$$props.style ? defaultStyle : $$props.style}
+    />
+{:then value}
     <img
         src={user?.profile?.image}
         alt={`Avatar for ${npub}`}
+        title={`Avatar for ${npub}`}
+        class={$$props.class}
+        style={!$$props.class && !$$props.style ? defaultStyle : $$props.style}
+    />
+{:catch error}
+    <img
+        src="https://placehold.co/400/faa/faa/webp"
+        alt={`Error loading avatar for ${npub}`}
+        title={`Error loading avatar for ${npub}`}
         class={$$props.class}
         style={!$$props.class && !$$props.style ? defaultStyle : $$props.style}
     />
