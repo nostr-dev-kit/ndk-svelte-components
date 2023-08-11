80c1b5b57962ab68b32afbfce7758a6b5c915df0
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-08-11 16:34:49 +0200

    Remove title from 30023

--- src/lib/event/content/Kind30023.svelte
+++ src/lib/event/content/Kind30023.svelte
@@ -20,35 +20,29 @@
 </script>
 
 <div class="article">
-    <h1 class="article-title">{article.title}</h1>
-    <div class="article-content">
-        {#each parsed as { type, value }, i}
-            {#if type === NEWLINE}
-                <NoteContentNewline {value} />
-            {:else if type === HTML}
-                {@html value}
-            {:else if type === TOPIC}
-                <NoteContentTopic {value} />
-            {:else if type === LINK}
-                <NoteContentLink {value} {showMedia} />
-            {:else if type.match(/^nostr:np(rofile|ub)$/)}
-                <NoteContentPerson {ndk} {value} on:click />
-            {:else if type.startsWith("nostr:") && showMedia && value.id !== anchorId}
-                <EventCard {ndk} id={value.id} relays={value.relays} />
-            {:else if type.startsWith("nostr:")}
-                <!-- <NoteContentEntity {value} /> -->
-            {:else}
-                {@html value}
-            {/if}
-        {/each}
-    </div>
+    {#each parsed as { type, value }, i}
+        {#if type === NEWLINE}
+            <NoteContentNewline {value} />
+        {:else if type === HTML}
+            {@html value}
+        {:else if type === TOPIC}
+            <NoteContentTopic {value} />
+        {:else if type === LINK}
+            <NoteContentLink {value} {showMedia} />
+        {:else if type.match(/^nostr:np(rofile|ub)$/)}
+            <NoteContentPerson {ndk} {value} on:click />
+        {:else if type.startsWith("nostr:") && showMedia && value.id !== anchorId}
+            <EventCard {ndk} id={value.id} relays={value.relays} />
+        {:else if type.startsWith("nostr:")}
+            <!-- <NoteContentEntity {value} /> -->
+        {:else}
+            {@html value}
+        {/if}
+    {/each}
 </div>
 
 <style lang="postcss">
-    * > :global(.article-content p) {
-    }
-
-    * > :global(.article-content img) {
+    * > :global(.article img) {
         object-fit: contain;
         width: 100%;
         height: 100%;
