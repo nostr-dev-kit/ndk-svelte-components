b646658cef08f758db1651f12c6f26aa9f92c8a0
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-08-16 11:49:45 +0200

    WIP: Add Npub and UserCard components

---
+++ src/lib/user/Npub.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+    import type { NDKUser } from "@nostr-dev-kit/ndk";
+    import type NDK from "@nostr-dev-kit/ndk";
+    import { truncatedNpub, copyToClipboard } from "$lib/utils/user";
+    import { Copy, Check } from 'lucide-svelte'
+    import { fade } from "svelte/transition";
+
+
+    /**
+     * The NDK instance you want to use
+     */
+    export let ndk: NDK;
+
+    /**
+     * The npub of the user you want to display an avatar for
+     */
+    export let npub: string | undefined = undefined;
+
+    /**
+     * The hexpubkey of the user you want to display an avatar for
+     */
+    export let pubkey: string | undefined = undefined;
+
+    /**
+     * The user object of the user you want to display an avatar for
+     */
+    export let user: NDKUser | undefined = undefined;
+
+    if (!user) {
+        let opts = npub ? { npub } : { hexpubkey: pubkey };
+        user = ndk.getUser(opts);
+        npub = user.npub;
+    }
+
+    let checkVisible = false;
+
+    function copyNpub() {
+        copyToClipboard(npub);
+        showCheck();
+    }
+
+    function showCheck() {
+        checkVisible = true;
+        setTimeout(() => {
+            checkVisible = false;
+        }, 1000);
+    }
+</script>
+
+<span class="name">
+    {#if user && user.npub}
+        <span class="npub {$$props.class}" style={$$props.style}>
+            {truncatedNpub(user)}
+            <button on:click|preventDefault={copyNpub} class="npub--copyButton">
+                <Copy size=16 />
+            </button>
+            {#if checkVisible}
+                <span in:fade={{duration: 100}} out:fade>
+                    <Check size=16 />
+                </span>
+            {/if}
+        </span>
+    {:else}
+        <span class="npub--error {$$props.class}" style={$$props.style}>
+            Error loading user
+        </span>
+    {/if}
+</span>
+
+<style lang="postcss">
+    .npub {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.5rem;
+    }
+
+    .npub--copyButton {
+        border: none;
+        padding: 0.25rem;
+        background-color: var(--color-bg);
+        cursor: pointer;
+    }
+</style>
--- src/lib/utils/user/index.ts
+++ src/lib/utils/user/index.ts
@@ -1,4 +1,4 @@
-import type { NDKUserProfile } from "@nostr-dev-kit/ndk";
+import type { NDKUser, NDKUserProfile } from "@nostr-dev-kit/ndk";
 
 export function truncatedNip05(userProfile: NDKUserProfile | undefined): string | undefined {
     if (!userProfile || !userProfile.nip05) return undefined;
@@ -8,4 +8,20 @@ export function truncatedNip05(userProfile: NDKUserProfile | undefined): string 
     } else {
         return userProfile.nip05;
     }
+}
+
+export function truncatedBech(bech32: string, length?: number): string {
+    return `${bech32.substring(0, length || 9)}...`;
+}
+
+export async function copyToClipboard(textToCopy: string | undefined) {
+    try {
+        await navigator.clipboard.writeText(textToCopy as string);
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+}
+
+export function truncatedNpub(user: NDKUser): string {
+    return user.npub ? truncatedBech(user.npub, 16) : '';
 }
