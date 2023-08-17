ea7b3159fc1fb8b07d61c6901e6ef8d441d244e9
pablof7z <p@f7z.io>
2023-08-17 12:24:58 +0300

    fix incorrect nip05 calculation

--- src/lib/utils/user/index.ts
+++ src/lib/utils/user/index.ts
@@ -1,12 +1,11 @@
 import type { NDKUserProfile } from "@nostr-dev-kit/ndk";
 
 export function truncatedNip05(userProfile: NDKUserProfile | undefined): string | undefined {
-    // Return blank string if we don't have a NIP-05
     if (!userProfile || !userProfile.nip05) return undefined;
 
-    if (userProfile.nip05.substring(0, 1) === "_") {
-        return userProfile.nip05.substring(2) as string;
+    if (userProfile.nip05.startsWith("_@")) {
+        return userProfile.nip05.substring(2);
     } else {
-        return userProfile.nip05 as string;
+        return userProfile.nip05;
     }
 }
