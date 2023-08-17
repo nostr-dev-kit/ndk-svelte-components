35bc6bb110c4003a5ed66be18571a2e5b4734e78
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-08-17 16:28:39 +0200

    Fix merge conflicts

--- src/lib/user/Avatar.svelte
+++ src/lib/user/Avatar.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-<<<<<<< HEAD
-    import type { NDKUser, NDKUserProfile } from '@nostr-dev-kit/ndk';
-    import type NDK from '@nostr-dev-kit/ndk';
-=======
     import type { NDKUser, NDKUserProfile } from "@nostr-dev-kit/ndk";
     import type NDK from "@nostr-dev-kit/ndk";
->>>>>>> 1c96436 (Update to pass userProfile data where possible.)
 
     /**
      * The NDK instance you want to use
@@ -28,19 +23,11 @@
     export let user: NDKUser | undefined = undefined;
 
     /**
-<<<<<<< HEAD
-     * The user profile to display an avatar for
-     */
-    export let userProfile: NDKUserProfile | undefined = undefined;
-
-    if (!user) {
-=======
      * An NDKUserProfile object for the user you want to display an avatar for
      */
     export let userProfile: NDKUserProfile | undefined = undefined;
 
     if (!userProfile && !user) {
->>>>>>> 1c96436 (Update to pass userProfile data where possible.)
         let opts = npub ? { npub } : { hexpubkey: pubkey };
         try {
             user = ndk.getUser(opts);
@@ -53,49 +40,30 @@
         if (userProfile) {
             resolve(userProfile);
         } else if (user) {
-            user.fetchProfile().then(() => {
-                userProfile = user!.profile;
-                resolve(userProfile);
-            }).catch(reject);
+            user.fetchProfile()
+                .then(() => {
+                    userProfile = user?.profile;
+                    resolve(userProfile!);
+                })
+                .catch(reject);
         } else {
             reject(`no user`);
         }
     });
 </script>
 
-<<<<<<< HEAD
 {#await fetchProfilePromise}
-    <img
-        alt=""
-        class="avatar avatar--loading {$$props.class}"
-        style={$$props.style}
-    />
+    <img alt="" class="avatar avatar--loading {$$props.class}" style={$$props.style} />
 {:then userProfile}
     <img
-        src={userProfile?.image??"https://placehold.co/400/ccc/ccc/webp"}
-=======
-{#if userProfile}
-    <img
         src={userProfile.image ?? "https://placehold.co/400/ccc/ccc/webp"}
->>>>>>> 1c96436 (Update to pass userProfile data where possible.)
         alt=""
         class="avatar avatar--image {$$props.class}"
         style={$$props.style}
     />
-{:else}
-    {#await user?.fetchProfile()}
-        <img alt="" class="avatar avatar--loading {$$props.class}" style={$$props.style} />
-    {:then value}
-        <img
-            src={user?.profile?.image ?? "https://placehold.co/400/ccc/ccc/webp"}
-            alt=""
-            class="avatar avatar--image {$$props.class}"
-            style={$$props.style}
-        />
-    {:catch error}
-        <img alt="" class="avatar avatar--error {$$props.class}" style={$$props.style} />
-    {/await}
-{/if}
+{:catch error}
+    <img alt="" class="avatar avatar--error {$$props.class}" style={$$props.style} />
+{/await}
 
 <style lang="postcss">
     .avatar {
--- src/lib/user/Name.svelte
+++ src/lib/user/Name.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
-<<<<<<< HEAD
-    import type { NDKUser, NDKUserProfile } from '@nostr-dev-kit/ndk';
-    import type NDK from '@nostr-dev-kit/ndk';
-    import { truncatedNip05 } from '$lib/utils/user';
-    import { truncatedBech32 } from '$lib/utils';
-    import { createEventDispatcher } from 'svelte';
-=======
     import type { NDKUser, NDKUserProfile } from "@nostr-dev-kit/ndk";
     import type NDK from "@nostr-dev-kit/ndk";
     import { truncatedNip05 } from "$lib/utils/user";
     import { truncatedBech32 } from "$lib/utils";
     import { createEventDispatcher } from "svelte";
->>>>>>> 1c96436 (Update to pass userProfile data where possible.)
 
     /**
      * The NDK instance you want to use
@@ -34,78 +26,62 @@
     export let user: NDKUser | undefined = undefined;
 
     /**
-<<<<<<< HEAD
-     * The user profile to display an avatar for
-=======
      * An NDKUserProfile object for the user you want to display a name for
->>>>>>> 1c96436 (Update to pass userProfile data where possible.)
      */
     export let userProfile: NDKUserProfile | undefined = undefined;
 
-    if (!user) {
+    if (!userProfile && !user) {
         let opts = npub ? { npub } : { hexpubkey: pubkey };
         try {
             user = ndk.getUser(opts);
+            npub = user.npub;
         } catch (e) {
             console.error(`error trying to get user`, { opts }, e);
         }
     }
 
+    const fetchProfilePromise = new Promise<NDKUserProfile>((resolve, reject) => {
+        if (userProfile) {
+            resolve(userProfile);
+        } else if (user) {
+            user.fetchProfile()
+                .then(() => {
+                    userProfile = user!.profile;
+                    resolve(userProfile!);
+                })
+                .catch(reject);
+        } else {
+            reject(`no user`);
+        }
+    });
+
     const dispatch = createEventDispatcher();
 </script>
 
 <span class="name">
-<<<<<<< HEAD
-    {#if userProfile}
-=======
-    {#if userProfile && user}
->>>>>>> 1c96436 (Update to pass userProfile data where possible.)
+    {#await fetchProfilePromise}
+        <span class="name--loading {$$props.class}" style={$$props.style}>
+            [{truncatedBech32(npub)}]
+        </span>
+    {:then userProfile}
         <button
             class="name--button {$$props.class}"
             style={$$props.style}
             tabindex="0"
             on:click|preventDefault|stopPropagation={() => {
-<<<<<<< HEAD
-                dispatch('click', user);
-=======
-                dispatch("click", user);
->>>>>>> 1c96436 (Update to pass userProfile data where possible.)
+                dispatch("click", user || userProfile);
             }}
         >
             {userProfile.displayName ||
                 userProfile.name ||
                 truncatedNip05(userProfile) ||
-<<<<<<< HEAD
-                truncatedBech32(_npub)}
-=======
-                truncatedBech32(user.npub)}
->>>>>>> 1c96436 (Update to pass userProfile data where possible.)
+                truncatedBech32(npub)}
         </button>
-    {:else if user}
-        {#await user.fetchProfile()}
-            <span class="name--loading {$$props.class}" style={$$props.style}>
-                [{truncatedBech32(user.npub)}]
-            </span>
-        {:then value}
-            <button
-                class="name--button {$$props.class}"
-                style={$$props.style}
-                tabindex="0"
-                on:click|preventDefault|stopPropagation={() => {
-                    dispatch("click", user);
-                }}
-            >
-                {user.profile?.displayName ||
-                    user.profile?.name ||
-                    truncatedNip05(user.profile) ||
-                    truncatedBech32(user.npub)}
-            </button>
-        {:catch error}
-            <span class="name--error {$$props.class}" style={$$props.style}>
-                [{truncatedBech32(user.npub)}]
-            </span>
-        {/await}
-    {/if}
+    {:catch error}
+        <span class="name--error {$$props.class}" style={$$props.style}>
+            [{truncatedBech32(npub)}]
+        </span>
+    {/await}
 </span>
 
 <style lang="postcss">
